99390ecf12ab2d34c809c3990799fc0ac6608c3a
Diego Demarco <demarkera@gmail.com>
2015-06-30 11:14:04 -0300

    code beautifier

--- manar.js
+++ manar.js
@@ -1,18 +1,19 @@
-    function switcher(divId){
-       $("div#" + divId).toggle();
-    }
-    function switcherAux(divId, value){
-        //alert(value);
-    	$("#aux").toggle();
-		$("#aux_value").show();
-		$("#aux_legend").text(value);
-        $("#aux_value").html($("div#" + divId).html());  
-		//$("div#" + divId).show();
+    function switcher(divId) {
+        $("div#" + divId).toggle();
     }
 
-    $("span").click( function(){
+    function switcherAux(divId, value) {
+        //alert(value);
+        $("#aux").toggle();
+        $("#aux_value").show();
+        $("#aux_legend").text(value);
+        $("#aux_value").html($("div#" + divId).html());
+        //$("div#" + divId).show();
+    }
+
+    $("span").click(function() {
         switcher(this.id);
     });
-    $("mark").click( function(){
+    $("mark").click(function() {
         switcherAux(this.id, $(this).text());
-    });    
+    });
