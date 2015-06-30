a2f319bb9dd046c03e4558f5444a216e7b2061c6
Diego Demarco <demarkera@gmail.com>
2015-06-30 11:48:35 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+var db = new PouchDB('dbname');
+
+db.put({
+  _id: 'dave@gmail.com',
+  name: 'David',
+  age: 68
+});
+
+db.changes().on('change', function() {
+  console.log('Ch-Ch-Changes');
+});
+
+db.replicate.to('http://demarkera.github.io/manaar/data.txt');
