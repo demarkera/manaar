3f4da8e3cbb955a65d307e5b86b7ee58866a33c7
Diego Demarco <demarkera@gmail.com>
2015-07-05 18:59:46 -0300

    jogos.htm

--- manar.js
+++ manar.js
@@ -3,12 +3,10 @@
     }
 
     function switcherAux(divId, value) {
-        //alert(value);
         $("#aux").toggle();
         $("#aux_value").show();
         $("#aux_legend").text(value);
         $("#aux_value").html($("div#" + divId).html());
-        //$("div#" + divId).show();
     }
 
     $("span").click(function() {
